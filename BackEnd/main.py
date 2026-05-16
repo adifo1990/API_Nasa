2a61609ff932790c76d3bee60defe38053ed3643
DY4M3S <dyamespdu@gmail.com>
2026-05-16 19:42:26 -0300

    Tela parcial APOD

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,20 +1,27 @@
 from fastapi import FastAPI
+
 import requests
 
 app = FastAPI()
 
 API_KEY = "hzBb8xbURx2RqGRLfy8b1JrNrfIcuA2RgYbZFGWw"
 
-@app.get("/foto/{data}")
-def buscar_foto(data: str):
+
+@app.get("/apod/fotos")
+def buscar_foto(date: str):
+    print("Data recebida:", date)
 
     url = "https://api.nasa.gov/planetary/apod"
 
     params = {
         "api_key": API_KEY,
-        "date": data
+        "date": date
     }
 
+    print("params:", params)
+
     resposta = requests.get(url, params=params)
+
+    print("resposta:", resposta)
 
     return resposta.json()
