ec538e44550af8cb4e5b9a9227304ee1c0e51fb9
andre <andrefazion52@gmail.com>
2026-05-24 16:12:38 -0300

    Neo quase terminada

--- FrontEnd/js/services.js
+++ FrontEnd/js/services.js
@@ -33,33 +33,54 @@ document.getElementById("BuscarData")
 
 });
 
+/*NeoWs*/
+
+document.getElementById("BuscarIntervalo")
+.addEventListener("click", async () => {
+
+    const dataInicial = document.getElementById("CampoDataInicial").value;
+    const dataFinal = document.getElementById("CampoDataFinal").value;
+
+    console.log("Data enviada:", dataInicial);
+
+    if (!dataInicial) {
+        alert("Informe uma data inical");
+        return;
+    }
+    if (!dataFinal) {
+        alert("Informe uma data final");
+        return;
+    }
+
+    try {
+        console.log(dataInicial);
+
+        const resposta = await fetch(
+           `https://api.nasa.gov/neo/rest/v1/feed?start_date=${dataInicial}&end_date=${dataFinal}`
+        );
+
+        const dados = await resposta.json();
+
+        console.log("dados da chamada rest:", dados);
+
+        mostrarImagem(dados);
+
+    } catch (erro) {
+        console.log("Erro:", erro);
+    }
+});
 
 function mostrarImagem(dados) {
 
     resultado.innerHTML = `
-    
-        <h3>${dados.title}</h3>
+        <div>
+            <h3>${dados.title}</h3>
 
-        <img 
-            src="${dados.url}" 
-            alt="${dados.title}"
-            width="500"
-        >
+            <img 
+                src="${dados.url}" 
+                alt="${dados.title}"
+                width="500"
+            >
+        </div>
     `;
 }
-
-/*function mostrarImagem(dados) {
-
-    resultado.innerHTML = `
-    
-        <h2>${dados.title}</h2>
-
-        <img 
-            src="${dados.url}" 
-            alt="${dados.title}"
-            width="500"
-        >
-
-         <p>${dados.explanation}</p>
-
-    `;}*/
