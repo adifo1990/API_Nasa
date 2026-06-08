ecfb27d6c97043e831d1fe4eddf1ce2999b78bf0
andre <andrefazion52@gmail.com>
2026-06-07 22:11:15 -0300

    front APOD e Neo (sem validação ou comunicação)

---
+++ FrontEnd/src/cartoes/neo/cartao_neo.tsx
@@ -0,0 +1,55 @@
+function cartao_neo() {
+
+    return(
+        <section className='cartão' id="neo">
+            <h2>Asteroids - NeoWs</h2>
+            <p>RESUMO:<br/>
+                - Near Earth Object Web Service -NeoWs-.<br/>
+                - Um serviço web RESTful para informações sobre asteroides próximos à Terra.
+            </p>
+            
+            <h3>Modos de pesquisa:</h3>
+            <ul>
+                <li>
+                    <form>
+                        <input type='hidden' id='neo_radio_intrevalo' value='neo_intervalo' name='modo_neo' />
+                        <p><b>Pesquisa por Intervalo</b></p>
+                        <hr />
+                        <label htmlFor='neo_input_data_inicial'>Data Incial*</label>
+                        <input type='date' id='neo_input_data_inicial' required />
+
+                        <label htmlFor="neo_input_data_final">Data Final*</label>
+                        <input type='date' id='neo_input_data_final' required />
+
+                        <input type='submit' value='Buscar' />
+                    </form>
+                </li>
+
+                <li>
+                    <form>
+                        <input type='hidden' id='neo_radio_id' value='neo_id' name='modo_neo' />
+                        <p><b>Pesquisa por ID</b></p>
+                        <hr />
+                        <label htmlFor='neo_input_id'>ID*</label>
+                        <input type='text' id='neo_input_id' placeholder='2001620' required />
+
+                        <input type='submit' value='Buscar' />
+                    </form>
+                </li>
+
+                <li>
+                    <form>
+                        <input type='hidden' id='neo_radio_browse' value='neo_browse' name='modo_neo' />
+                        <p><b>listar todos</b></p>
+                        <hr />
+                        <input type='submit' value='Buscar' />
+                    </form>
+                </li>
+            </ul>
+            <div id='resposta'>
+
+            </div>
+        </section>
+    )
+}
+export default cartao_neo
